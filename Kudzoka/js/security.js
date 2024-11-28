7553512855a1acf71b1a32f29b50bbd31d9b19f2
ahmdxsdk <166481859+ahmdxsdk@users.noreply.github.com>
2024-11-28 03:12:23 +0200

    Create security.js

---
+++ Kudzoka/js/security.js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
